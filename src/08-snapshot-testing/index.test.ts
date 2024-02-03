5d242cbdf999306ce0899859ec403c13d6912752
Tedzury <asdrogachev@gmail.com>
2024-02-03 13:41:31 +0200

    feat: finish the snapshot section

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -4,12 +4,26 @@ import { generateLinkedList } from './index';
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    const res = generateLinkedList([1]);
-    console.log(res);
+    const expectation = {
+      value: 'a',
+      next: {
+        value: 'b',
+        next: {
+          value: 'c',
+          next: {
+            value: 'd',
+            next: { value: 'e', next: { value: null, next: null } },
+          },
+        },
+      },
+    };
+    const res = generateLinkedList(['a', 'b', 'c', 'd', 'e']);
+    expect(res).toStrictEqual(expectation);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const res = generateLinkedList(['a', 'b', 'c', 'd', 'e']);
+    expect(res).toMatchSnapshot();
   });
 });
