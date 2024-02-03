8de4a04bab5cd909c3b0ed86eb8ff25cc410501f
Tedzury <asdrogachev@gmail.com>
2024-02-03 12:38:32 +0200

    feat: finish the mocking api lib section

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,10 +1,11 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const res = generateLinkedList([1]);
+    console.log(res);
   });
 
   // Check match by comparison with snapshot
